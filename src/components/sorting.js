f8891afdeece5a8d1d08fbb3f622484965a021fa
Rina Akinina <rinaakinina@yandex.ru>
2026-02-25 17:52:46 +0300

    smart-table

--- src/components/sorting.js
+++ src/components/sorting.js
@@ -1,18 +1,30 @@
-import {sortCollection, sortMap} from "../lib/sort.js";
+import { sortCollection, sortMap } from "../lib/sort.js";
 
 export function initSorting(columns) {
-    return (data, state, action) => {
-        let field = null;
-        let order = null;
+  return (data, state, action) => {
+    let field = null;
+    let order = "none";
 
-        if (action && action.name === 'sort') {
-            // @todo: #3.1 — запомнить выбранный режим сортировки
+    if (action && action.name === "sort") {
+      // #3.1 — запомнить выбранный режим сортировки
+      action.dataset.value = sortMap[action.dataset.value ?? "none"];
 
-            // @todo: #3.2 — сбросить сортировки остальных колонок
-        } else {
-            // @todo: #3.3 — получить выбранный режим сортировки
-        }
+      field = action.dataset.field;
+      order = action.dataset.value;
 
-        return sortCollection(data, field, order);
+      // #3.2 — сбросить сортировки остальных колонок
+      columns.forEach((btn) => {
+        if (btn !== action) btn.dataset.value = "none";
+      });
+    } else {
+      // #3.3 — получить выбранный режим сортировки
+      const active = columns.find((btn) => btn.dataset.value !== "none");
+      if (active) {
+        field = active.dataset.field;
+        order = active.dataset.value;
+      }
     }
+
+    return sortCollection(data, field, order);
+  };
 }
